0c44d38f0252d9b50b71d6771ec18e76408cce96
Lauren <145627044+Laursaurus@users.noreply.github.com>
2024-09-28 01:12:28 -0500

    Player movement, platforms, and backround draft
    Player movement - Jump and horizontal movment mechanic

---
+++ Gamejam-2024-5/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpSpeed;
+    private Rigidbody2D body;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        //Jump
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
